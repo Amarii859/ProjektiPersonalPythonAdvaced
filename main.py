698b2a436681c193216610cdc5d95bd927a2e2c9
Hasan Suxhuku <amarbuzoku123@gmail.com>
2026-06-24 12:39:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from fastapi import FastAPI
-from crud import get_students
+from crud import (
+    get_students,
+    add_student,
+    update_student,
+    delete_student
+)
 
 app = FastAPI()
+
 
 @app.get("/students")
 def read_students():
     return get_students()
+
+
+@app.post("/students")
+def create_student(
+    name: str,
+    surname: str,
+    class_name: str,
+    age: int
+):
+    add_student(name, surname, class_name, age)
+    return {"message": "Student added"}
+
+
+@app.put("/students/{student_id}")
+def edit_student(
+    student_id: int,
+    name: str,
+    surname: str,
+    class_name: str,
+    age: int
+):
+    update_student(
+        student_id,
+        name,
+        surname,
+        class_name,
+        age
+    )
+    return {"message": "Student updated"}
+
+
+@app.delete("/students/{student_id}")
+def remove_student(student_id: int):
+    delete_student(student_id)
+    return {"message": "Student deleted"}
 
