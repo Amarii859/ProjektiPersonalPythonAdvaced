3af2805782dfbc935992060df1ecee6e93740c89
Hasan Suxhuku <amarbuzoku123@gmail.com>
2026-06-24 12:42:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 from fastapi import FastAPI
+from schemas import Student
 from crud import (
     get_students,
     add_student,
@@ -9,42 +11,39 @@ from crud import (
 app = FastAPI()
 
 
+# GET all students
 @app.get("/students")
 def read_students():
     return get_students()
 
 
+# POST student
 @app.post("/students")
-def create_student(
-    name: str,
-    surname: str,
-    class_name: str,
-    age: int
-):
-    add_student(name, surname, class_name, age)
-    return {"message": "Student added"}
+def create_student(student: Student):
+    add_student(
+        student.name,
+        student.surname,
+        student.class_name,
+        student.age
+    )
+    return {"message": "Student added successfully"}
 
 
+# PUT student
 @app.put("/students/{student_id}")
-def edit_student(
-    student_id: int,
-    name: str,
-    surname: str,
-    class_name: str,
-    age: int
-):
+def edit_student(student_id: int, student: Student):
     update_student(
         student_id,
-        name,
-        surname,
-        class_name,
-        age
+        student.name,
+        student.surname,
+        student.class_name,
+        student.age
     )
-    return {"message": "Student updated"}
+    return {"message": "Student updated successfully"}
 
 
+# DELETE student
 @app.delete("/students/{student_id}")
 def remove_student(student_id: int):
     delete_student(student_id)
-    return {"message": "Student deleted"}
-
+    return {"message": "Student deleted successfully"}
